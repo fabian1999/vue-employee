4624d7893977b7e9b409002d549f0537dd31e978
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-06 11:19:31 +0300

    Added EmployeeList component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <Filters />
-  <Form />
+  <div class="form-container">
+    <Form />
+    <EmployeeList />
+  </div>
 </template>
 
 <script>
 import Filters from "./components/Filters.vue";
 import Form from "./components/Form.vue";
+import EmployeeList from "./components/EmployeeList.vue";
 
 export default {
   name: "App",
   components: {
     Filters,
-    Form
+    Form,
+    EmployeeList,
   },
 };
 </script>
@@ -21,8 +26,30 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #eeeeee;
+  overflow: hidden;
+}
+
+@media only screen and (max-width: 1200px) {
+  body,
+  html {
+    overflow: auto;
+  }
+}
+
+.form-container {
+  width: 92%;
+  margin-left: 8%;
+  margin-top: 0%;
 }
 
 .styled-buttons {
@@ -33,5 +60,9 @@ export default {
   border: none;
   background-color: #f8f9fa;
   border-radius: 5px;
+}
+
+.styled-buttons:hover {
+  box-shadow: 0px 0px 1px 1px #000000;
 }
 </style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list">
+    <table id="table">
+      <tr>
+        <th>Last Name</th>
+        <th>First Name</th>
+        <th>Email</th>
+        <th>Sex</th>
+        <th>Date</th>
+        <th>Image</th>
+        <th>Delete</th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmployeeList",
+};
+</script>
+
+<style scoped>
+#table {
+  width: 120vh;
+}
+
+.list {
+  width: 130vh;
+  height: 80vh;
+  background-color: white;
+  display: grid;
+  justify-content: center;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px 1px #000000;
+}
+
+td {
+  text-align: center;
+}
+</style>
