d4a7165a3a31de671423d2ba87b23fa5e1a649dd
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-06 13:23:55 +0300

    Added fetch to get employees from database.

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -15,8 +15,66 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
   name: "EmployeeList",
+  data() {
+    return {
+      employeesList: [],
+    };
+  },
+  async created() {
+    let self = this;
+    $.ajax({
+      method: "GET",
+      url: "https://localhost:5001/employee/Employee",
+      success: function(data) {
+        this.employeesList = data;
+        self.loadEmployees(this.employeesList);
+      },
+      error: function() {
+        alert(`Failed to get employees list.`);
+      },
+    });
+  },
+  methods: {
+    loadEmployees(employeesList) {
+      for (var index = 0; index < employeesList.length; index++) {
+        this.appendRow(employeesList[index]);
+      }
+    },
+    appendRow(employee) {
+      let employeesTable = document.getElementById("table");
+      var id = employeesTable.getElementsByTagName("tr").length;
+
+      employeesTable.innerHTML += `<tr><td>
+        ${employee.lastName}
+        </td><td>
+        ${employee.firstName}
+        </td><td>
+        ${employee.email}
+        </td><td>
+        ${employee.gender}
+        </td><td>
+        ${employee.birthdate}
+        </td><td>
+        <img id='image" ${id} 'style='width: 20px; height: 20px' src='#'></img></td><td><button onClick='this.deleteUser(this, ${employee.id})'>X</button></td></tr>`;
+    },
+    deleteUser(btn, idFromDb) {
+      var row = btn.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+
+      $.ajax({
+        method: "DELETE",
+        url: `https://localhost:5001/employee/Employee/${idFromDb}`,
+        success: function() {},
+        error: function() {
+          alert(`Failed to delete employee.`);
+        },
+      });
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import $ from 'jquery'
 
 createApp(App).mount('#app')
